025084d4ce754cadffed6b9ba298fa77515d5107
n1nj4r007 <ninjaroot@localhost.localdomain>
2019-10-14 12:31:09 -0400

    make it better

--- bootcampproject/settings.py
+++ bootcampproject/settings.py
@@ -110,3 +110,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+import django_heroku
+django_heroku.settings(locals())
